f6b84f45903da3e281251b2fcef9160fdec7953f
abhaymishra123 <abhay.mishra@trademo.com>
2023-12-17 23:20:02 +0530

    updated shopynglyx to shoppingcart

--- ShoppingCart/wsgi.py
+++ ShoppingCart/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for shoppinglyX project.
+WSGI config for ShoppingCart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shoppinglyX.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShoppingCart.settings')
 
 application = get_wsgi_application()
